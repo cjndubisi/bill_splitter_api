de118862beabdb37f03e30336e0ac246273f1d18
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-14 13:02:04 +0100

    chore: migrate to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 var router = express.Router();
-
 
 router.get('/', function (req, res) {
   res.json({ message: 'Root Page' });
@@ -16,10 +15,10 @@ router.get('/', function (req, res) {
 // routes
 app.use('/v1', router);
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.status(400).json({ message: 'Bad Request' });
 });
 
 app.listen(port, () => {
-  console.log(`Successfully started server at port ${port}`)
+  console.log(`Successfully started server at port ${port}`);
 });
