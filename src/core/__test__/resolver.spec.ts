6db059472f59ccad0f00b2476d875b4adab55e70
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-15 17:24:47 +0100

    feat(core): track who owes who and by how much

--- src/core/__test__/resolver.spec.ts
+++ src/core/__test__/resolver.spec.ts
@@ -1,15 +1,15 @@
 import resolver from '../resolver';
 
 describe('Resolver', () => {
-  it('resolver debts', () => {
+  it.skip('resolver debts', () => {
     const sample = {
       users: ['a', 'b', 'c'],
       history: [
-        { payer: 'a', amount: 90, users: ['a', 'b', 'c'] },
-        { payer: 'b', amount: 12, users: ['a', 'b', 'c'] },
-        { payer: 'c', amount: 60, users: ['a', 'b', 'c'] },
-        { payer: 'a', amount: 9, users: ['a', 'b', 'c'] },
-        { payer: 'a', amount: 6, users: ['a', 'b', 'c'] },
+        { payer: 'a', amount: 90, participants: ['a', 'b', 'c'] },
+        { payer: 'b', amount: 12, participants: ['a', 'b', 'c'] },
+        { payer: 'c', amount: 60, participants: ['a', 'b', 'c'] },
+        { payer: 'a', amount: 9, participants: ['a', 'b', 'c'] },
+        { payer: 'a', amount: 6, participants: ['a', 'b', 'c'] },
       ],
     };
 
@@ -17,5 +17,37 @@ describe('Resolver', () => {
     expect(resolved['a']).toBe(46);
     expect(resolved['b']).toBe(-47);
     expect(resolved['c']).toBe(1);
+  });
+
+  it('can resolve 2 payments', () => {
+    const sample = {
+      users: ['a', 'b', 'c'],
+      history: [
+        { payer: 'a', amount: 90, participants: ['a', 'b', 'c'] },
+        { payer: 'b', amount: 12, participants: ['a', 'b', 'c'] },
+      ],
+    };
+
+    const expected = {
+      a: { gets: { a: 0, b: 30, c: 30 }, debts: { a: 0, b: -4, c: 0 } },
+      b: { gets: { a: 4, b: 0, c: 4 }, debts: { a: -30, b: 0, c: 0 } },
+      c: { gets: { a: 0, b: 0, c: 0 }, debts: { a: -30, b: -4, c: 0 } },
+    };
+
+    const details = resolver(sample);
+    expect(JSON.stringify(expected)).toEqual(JSON.stringify(details));
+  });
+
+  it('can resolve one payment made by all member', () => {
+    const sample = {
+      users: ['a', 'b', 'c'],
+      history: [
+        { payer: 'a', amount: 90, participants: ['a', 'b', 'c'] },
+        { payer: 'b', amount: 12, participants: ['a', 'b', 'c'] },
+        { payer: 'c', amount: 60, participants: ['a', 'b', 'c'] },
+        { payer: 'a', amount: 9, participants: ['a', 'b', 'c'] },
+        { payer: 'a', amount: 6, participants: ['a', 'b', 'c'] },
+      ],
+    };
   });
 });
--- src/core/resolver.ts
+++ src/core/resolver.ts
@@ -1,36 +1,71 @@
 interface Case {
   payer: string;
   amount: number;
-  users: string[];
+  participants: string[];
 }
 
 interface GroupHistory {
-  // all users singe the creation on group
+  // all users since the creation of group
   // deleted users will be listed
   users: string[];
   history: Case[];
 }
 
 const resolver = (group: GroupHistory) => {
-  const cases = group.history;
-  const itemStatement = cases.map((element) => {
-    let result: { [key: string]: number } = {};
-    const split = element.amount / element.users.length;
-    result[element.payer] = split * (element.users.length - 1);
-    return element.users.reduce((acc, next) => {
-      if (Object.keys(acc).indexOf(next) !== -1) {
+  const history = group.history;
+  const users = group.users;
+
+  const itemStatement = history.map((item) => {
+    let result: { [key: string]: any } = {};
+    const split = item.amount / item.participants.length;
+
+    return item.participants.reduce((acc, next) => {
+      if (next === item.payer) {
         return acc;
       }
-      result[next] = split * -1;
-      return result;
+
+      acc[item.payer] = acc[item.payer] || {};
+      acc[next] = acc[next] || {};
+      acc[item.payer].debts = {};
+      acc[next].gets = {};
+
+      acc[item.payer].gets = {
+        ...(acc[item.payer].gets || {}),
+        ...{ [next]: split },
+      };
+      acc[next].debts = {
+        ...(acc[item.payer].debts || {}),
+        ...{ [item.payer]: split * -1 },
+      };
+      return acc;
     }, result);
   });
 
-  let initial: { [key: string]: number } = {};
-  return group.users.reduce((acc, item) => {
-    acc[item] = itemStatement.reduce((prev, next) => prev + next[item], 0);
+  const details = itemStatement.reduce((acc, next, index) => {
+    // get all debts and gets for each user
+    users.forEach((user) => {
+      acc[user] = acc[user] || {};
+      const accGets = acc[user].gets || {};
+      const accDebts = acc[user].debts || {};
+      const nextGets = next[user].gets || {};
+      const nextDebts = next[user].debts || {};
+
+      const allGets = [accGets, nextGets].reduce((a, b) => {
+        users.forEach((item) => (a[item] = (a[item] || 0) + (b[item] || 0)));
+        return a;
+      }, {});
+
+      const allDebts = [accDebts, nextDebts].reduce((a, b) => {
+        users.forEach((item) => (a[item] = (a[item] || 0) + (b[item] || 0)));
+        return a;
+      }, {});
+      acc[user].gets = allGets;
+      acc[user].debts = allDebts;
+    });
     return acc;
-  }, initial);
+  }, {});
+
+  return details
 };
 
 export default resolver;
