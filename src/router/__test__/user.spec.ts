2cf5d9087a13dc45adae7a4a3188f87d9ef4b796
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-16 19:18:13 +0100

    fix: enable sql for test and fix failing specs

--- src/router/__test__/user.spec.ts
+++ src/router/__test__/user.spec.ts
@@ -1,16 +1,12 @@
 import request from 'supertest';
 import app from '../../server';
-jest.mock('sequelize');
-jest.mock('../../models/user', () => {
-  return {
-    createUser: jest.fn().mockReturnValue({
-      toJSON: () => ({ password: 'ff', email: 'sdd', name: 'lsss' }),
-    }),
-    findBy: jest.fn().mockReturnValue({
-      toJSON: () => ({ password: 'ff', email: 'sdd', name: 'lsss' }),
-      verifyPassword: () => true,
-    }),
-  };
+import db from './../../db';
+beforeAll(async () => {
+  await db.sequelize.sync();
+});
+
+afterAll(async () => {
+  await db.sequelize.query('DROP TABLE groups; DROP TABLE users;');
 });
 
 describe('User Route', () => {
@@ -18,10 +14,10 @@ describe('User Route', () => {
     it('fails on invalid param', async () => {
       const res = await request(app)
         .post('/v1/users/signup')
-        .send({ email: 'fasdf', password: 'fsdfs', name: 'safa' });
+        .send({ email: 'fasdf', password: 'fsdfs' });
+      console.log(res.body, res.status);
 
-      expect(res.body.message).not.toBeNull();
-      expect(res.body.message).toBe('Signup Successful');
+      expect(res.body.message).toBe('Bad Request');
     });
 
     it('can create account', async () => {
@@ -29,18 +25,18 @@ describe('User Route', () => {
         .post('/v1/users/signup')
         .send({ email: 'fasdf', password: 'fsdfs', name: 'safa' });
 
-      expect(res.body.message).not.toBeNull();
       expect(res.body.message).toBe('Signup Successful');
     });
   });
 
   describe('login', () => {
     it('can login', async () => {
+      await request(app)
+        .post('/v1/users/signup')
+        .send({ email: 'fasdf', password: 'fsdfs', name: 'safa' });
       const res = await request(app)
         .post('/v1/users/login')
-        .send({ email: 'fasdf', password: 'fsdfs' });
-      console.log(res.body, res.status);
-
+        .send({ email: 'fasdf', password: 'fsdfss' });
       expect(res.body.user).not.toBeNull();
       expect(res.body.token).not.toBeNull();
       expect(res.body.message).not.toBeNull();
--- src/router/user.ts
+++ src/router/user.ts
@@ -1,17 +1,41 @@
-import { Router } from 'express';
+import { Router, NextFunction } from 'express';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
-require('./../core/auth');
 
 const router = Router();
 
-router.post(
-  '/signup',
-  passport.authenticate('signup', { session: false }),
-  async (req, res) => {
-    res.status(201).json({ user: req.user, message: 'Signup Successful' });
-  }
-);
+const login = (
+  req: Express.Request,
+  res: any,
+  next: NextFunction,
+  { message, user }: { message: string; user: any }
+) => {
+  req.login(user, { session: false }, async (error) => {
+    if (error) {
+      return next(error);
+    }
+    const payload = { _id: user.id, email: user.email };
+    const token = jwt.sign({ user: payload }, process.env.JWT_SECRET);
+    res.json({
+      user,
+      token,
+      message,
+    });
+  });
+};
+
+router.post('/signup', async (req, res, next) => {
+  passport.authenticate('signup', { session: false }, (err, user, info) => {
+    try {
+      if (err || !user) {
+        return res.status(401).json({ message: 'Bad Request' });
+      }
+      return login(req, res, next, { user, message: 'Signup Successful' });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
 
 router.post('/login', async (req, res, next) => {
   passport.authenticate('login', { session: false }, (err, user, info) => {
@@ -20,14 +44,7 @@ router.post('/login', async (req, res, next) => {
         const error = new Error('Unable to authenticate');
         return next(error);
       }
-      req.login(user, { session: false }, async (error) => {
-        if (error) {
-          return next(error);
-        }
-        const payload = { _id: user.id, email: user.email };
-        const token = jwt.sign({ user: payload }, process.env.JWT_SECRET);
-        res.json({ user, token, message: 'Signup Successful' });
-      });
+      return login(req, res, next, { user, message: 'Login Successful' });
     } catch (error) {
       return next(error);
     }
