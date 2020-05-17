c7944659a70c3c1695b9027b7cae405bf3b64c4b
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-17 15:23:13 +0100

    fix: double authetication on group router

--- src/router/group.ts
+++ src/router/group.ts
@@ -1,14 +1,6 @@
 import { Router } from 'express';
-import Group, {
-  findBy,
-  createGroup,
-  deleteGroup,
-  UserAttributes,
-} from '../models/group';
-
-import passport from 'passport';
-import { userRouter } from './user';
-import { createUser } from '../models/user';
+import Group, { findBy, deleteGroup } from '../models/group';
+import { createUser, UserAttributes } from '../models/user';
 const router = Router();
 
 router.post('/', async (req, res) => {
@@ -56,42 +48,34 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    try {
-      const user: UserAttributes = req.user as UserAttributes;
-      const groups = await user.getGroups();
-      return res.status(200).json(groups);
-    } catch (error) {
-      return res.status(400).json({ message: error.message });
-    }
+router.get('/', async (req, res) => {
+  try {
+    const user: UserAttributes = req.user as UserAttributes;
+    const groups = await user.getGroups();
+    return res.status(200).json(groups);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
   }
-);
+});
 
-router.post(
-  '/:id/users',
-  passport.authenticate('jwt', { session: false }),
-  async ({ params, body }, res) => {
-    try {
-      const group: any = await Group.findOne({
-        where: { id: params.id },
-        include: [{ all: true }],
-      });
-      const userToAdd = body;
-      userToAdd.activated = false;
-      userToAdd.password = 'invalidatePassword';
+router.post('/:id/users', async ({ params, body }, res) => {
+  try {
+    const group: any = await Group.findOne({
+      where: { id: params.id },
+      include: [{ all: true }],
+    });
+    const userToAdd = body;
+    userToAdd.activated = false;
+    userToAdd.password = 'invalidatePassword';
 
-      const newUser = await createUser(userToAdd);
-      await group.addUsers(newUser);
-      await group.reload();
-      return res.status(201).json(group);
-    } catch (error) {
-      return res.status(400).json({ message: error.message });
-    }
+    const newUser = await createUser(userToAdd);
+    await group.addUsers(newUser);
+    await group.reload();
+    return res.status(201).json(group);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
   }
-);
+});
 
 export const groupRouter = router;
 export default router;
