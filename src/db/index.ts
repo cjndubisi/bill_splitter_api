c941a00bb286dcc5818f160945f17b12ab0913aa
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-18 12:39:10 +0100

    fix: heroku database url not specified

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,11 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.PG_URL, {
-  logging: process.env.NODE_ENV === 'development',
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || process.env.PG_URL,
+  {
+    logging: process.env.NODE_ENV === 'development',
+  }
+);
 export default {
   sequelize,
 };
