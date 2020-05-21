b6907a2047cd41fa3f5195479738f3bcf5d66b02
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-22 00:20:39 +0100

    add logger

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,6 +1,7 @@
 import { Router, NextFunction } from 'express';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
+import { logger } from '../logger';
 
 const router = Router();
 
@@ -11,6 +12,7 @@ const login = (
 ) => {
   req.login(user, { session: false }, async (error) => {
     if (error) {
+      logger.error(error, user, 'will return 400');
       return res.status(400).json({ message: error.message });
     }
     const payload = { _id: user.id, email: user.email };
@@ -27,14 +29,17 @@ router.post('/signup', async (req, res, next) => {
   passport.authenticate('signup', { session: false }, (err, user, info) => {
     try {
       if (err || !user) {
+        logger.info(err, user, 'will throw error');
         throw err;
       }
       return login(req, res, { user, message: 'Signup Successful' });
     } catch (error) {
+      logger.error(error);
       let message = 'Bad Request';
       if (error?.errors) {
         message = error.errors.map((error: any) => error.message)[0];
       }
+      logger.info(error, message);
       return res.status(400).json({ message });
     }
   })(req, res, next);
@@ -44,10 +49,12 @@ router.post('/login', async (req, res, next) => {
   passport.authenticate('login', { session: false }, (err, user, info) => {
     try {
       if (err || !user) {
+        logger.info(err, user, 'Unable to authenticate');
         throw new Error('Unable to authenticate');
       }
       return login(req, res, { user, message: 'Login Successful' });
     } catch (error) {
+      logger.info(error, 'Invalid email and password');
       return res.status(401).json({ message: 'Invalid email and password' });
     }
   })(req, res, next);
